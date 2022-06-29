f749dc45845f810a0085ea3a1024c0ad9cb27f31
Rodrigo Souza Silva <souza.rodrigo11@gmail.com>
2022-06-29 16:21:54 -0300

    Add files via upload

---
+++ Aula7.py
@@ -0,0 +1,10 @@
+from ast import For, In
+from sre_constants import IN
+def pão_de_açuçar():
+     for x in Vagas_Aberta:
+          print('Olá',Vagas_Aberta,'Estamos Com Vagas Em Aberta Gostaria De Se Candidatar ?')
+
+Vagas_Aberta = 'Rodrigo'
+pão_de_açuçar()
+
+
